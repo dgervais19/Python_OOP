fae7b40022169f370e86ffe35c2c714babaffc3a
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-05 11:23:39 +0000

    creating child class (Reptile) that inherits from parent class (Animal)

--- OOP/animal.py
+++ OOP/animal.py
@@ -1,1 +1,29 @@
 # Creating an Animal class as PARENT/BASE/SUPER class
+
+class Animal:
+
+    def __init__(self):  # initialising the Animal class
+        self.alive = True  # creating an attribute/variable
+        self.spine = True
+        self.lungs = True
+        self.eyes = True
+
+
+    # create behaviours as functions/methods
+    def breathe(self):
+        return  "Keep breathing to stay alive "
+
+    def move(self):
+        return "left to right and up and down...."
+
+    def eat(self):
+        return "Nom Nom Nom"
+
+    def procreate(self):
+        return "find partner"
+
+# instantiate our class/ create and object
+
+# cat = Animal() # creaeting an object of Animal class
+# cat as child has inherited everything from animal
+# print(cat.eat())
---
+++ OOP/reptile.py
@@ -0,0 +1,37 @@
+# creating a reptile class as a child class of our Animal class
+
+from animal import Animal
+
+class Reptile(Animal):
+    def __init__(self):
+
+
+        super().__init__()  # Super is a keyword is used to inherit everything from a Parent Class
+        self.cold_blooded = True
+        self.tetrapod = True
+        self.heart_chamber = [3, 4]
+
+    # creating function for our Reptile class
+    def seek_heat(self):
+        return "it's freezing, look for some warmth"
+
+    def hunt(self):
+        return "Work hard to catch a meal or go home hungry"
+
+    def use_venom(self):
+        return "Use it only when you need to"
+
+
+# lets create an object of our Reptile class to utilise the amazing functionalities of OOP
+reptile_object = Reptile()
+
+# Lets also see if we can still print the inherited attributes from the parent class
+print(reptile_object.eat())
+print(reptile_object.breathe())
+print(reptile_object.procreate())
+
+# Now lets look at the attributes from the current class
+print(reptile_object.seek_heat())
+print(reptile_object.hunt())
+print(reptile_object.use_venom())
+
